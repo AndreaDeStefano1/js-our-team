e97cb5adaa2c3af2f30dd0417cc3b443818fdb70
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-04-27 16:17:14 +0200

    Essercizio finito

--- script.js
+++ script.js
@@ -5,24 +5,68 @@ const teamMembers = [
     photo: "img/wayne-barnett-founder-ceo.jpg",
   }
 ];
-const target = document.querySelector('.team-container')
+const target = document.querySelector('.team-container');
+const button = document.getElementById('addMemberButton')
+// const card = document.createElement('div').classList.add('team-card');
+// const img = document.createElement('div').classList.add('card-image');
+// const text = document.createElement('div').classList.add('card-text');
+// const fullName = document.createElement('h3');
+// const role = document.createElement('p');
 
+function stampMember(){
+  for (let i = 0; i < teamMembers.length; i++) {
+    const element = teamMembers[i];
+    target.innerHTML += `
+    <div class="team-card">
+      <div class="card-image">
+        <img
+          src='${element.photo}'
+          alt="${element.name}"
+        />
+      </div>
+      <div class="card-text">
+        <h3>${element.name}</h3>
+        <p>${element.role}</p>
+      </div>
+    </div> `;
+  }
+}
 
-for (let i = 0; i < teamMembers.length; i++) {
-  const element = teamMembers[i];
-  target.innerHTML += 
-  `
+function stampNewMember (member){
+  target.innerHTML += `
   <div class="team-card">
     <div class="card-image">
       <img
-        src='${element.photo}'
-        alt="${element.name}"
+        src='${member.photo}'
+        alt="${member.name}"
       />
     </div>
     <div class="card-text">
-      <h3>${element.name}</h3>
-      <p>${element.role}</p>
+      <h3>${member.name}</h3>
+      <p>${member.role}</p>
     </div>
-  </div>
-  `
+  </div> `;
 }
+
+function createTeamMember(){
+
+  const newMember = new Object();
+
+  const getName = document.getElementById('name').value;
+  const getRole = document.getElementById('role').value;
+  const getImage = document.getElementById('image').value;
+
+  newMember.name = getName;
+  newMember.role = getRole;
+  newMember.photo = getImage;
+
+  teamMembers.push(newMember)
+  return newMember
+}
+
+stampMember();
+button.addEventListener('click', function (){
+  stampNewMember(createTeamMember());
+});
+
+
